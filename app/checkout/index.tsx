bf9386ecf97e6d02f5fbb62caaad7583990cc7c5
Odetundemay <88235457+Odetundemay@users.noreply.github.com>
2023-06-24 18:22:21 +0100

    navigation between screens

--- app/checkout/index.tsx
+++ app/checkout/index.tsx
@@ -1,12 +1,19 @@
-import { Link } from 'expo-router'
+import { useRouter } from 'expo-router'
 import { View, Text } from 'react-native'
+import { Button } from 'react-native-paper'
 
 export default function PersonalDetails() {
+  const router = useRouter()
+  const nextPage = () => {
+    router.push('/checkout/delivery')
+  }
   return (
     <View>
       <Text style={{ fontSize: 60 }}>Personal Details</Text>
 
-      <Link href={'/checkout/delivery'}>Next</Link>
+      <Button mode='contained' onPress={nextPage}>
+        Next
+      </Button>
     </View>
   )
 }
--- app/checkout/payment.tsx
+++ app/checkout/payment.tsx
@@ -1,9 +1,19 @@
+import { useRouter } from 'expo-router'
 import { View, Text } from 'react-native'
+import { Button } from 'react-native-paper'
 
 export default function PaymentDetails() {
+  const router = useRouter()
+  const nextPage = () => {
+    //Submit
+    router.push('/')
+  }
   return (
     <View>
       <Text style={{ fontSize: 30 }}>Payment Details</Text>
+      <Button onPress={nextPage} mode='contained'>
+        Submit
+      </Button>
     </View>
   )
 }
